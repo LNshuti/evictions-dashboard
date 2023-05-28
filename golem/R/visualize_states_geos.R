fc5ce528ec9eeeaa66123362e28c4c93dc812221
Leonce Nshuti <lnshuti@Leonces-Mac-mini.local>
2023-05-27 22:04:34 -0500

    update local

--- golem/R/visualize_states_geos.R
+++ golem/R/visualize_states_geos.R
@@ -3,6 +3,27 @@ library(sf)
 library(here)
 library(tidyverse)
 
+Sys.setenv(
+  AWS_ACCESS_KEY_ID = my_access_key,
+  AWS_SECRET_ACCESS_KEY = my_secret_key,
+  AWS_REGION = my_region
+)
+
+
+# Connect to S3 using temporary security credentials.
+s3_other <- paws::s3(
+  config = list(
+    credentials = list(
+      creds = list(
+        access_key_id = my_access_key,
+        secret_access_key = my_secret_key,
+        session_token = my_token
+      )
+    ),
+    region = my_region
+  )
+)
+
 # Load shape files
 sf_state <- read_sf(here("golem/R/output/state/01_state-shape-file.shp")) %>% 
   st_transform(crs = 4326) 
