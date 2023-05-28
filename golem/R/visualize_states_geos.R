5468684eb07132a6e0f883d221ec2c7242a561e8
Leonce Nshuti <lnshuti@Leonces-Mac-mini.local>
2023-05-27 22:03:03 -0500

    update local

--- golem/R/visualize_states_geos.R
+++ golem/R/visualize_states_geos.R
@@ -1,5 +1,8 @@
 library(ggrepel)
+library(sf)
+library(here)
+library(tidyverse)
 
 # Load shape files
-sf_state <- read_sf(here("output/state/01_state-shape-file.shp")) %>% 
+sf_state <- read_sf(here("golem/R/output/state/01_state-shape-file.shp")) %>% 
   st_transform(crs = 4326) 
